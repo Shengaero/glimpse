aadd80a2aeeba4e23114faf6e67c3ced24f71f99
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-11-11 13:30:47 -0600

    finished redirects and logout function

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Chat from '../components/Chat';
 import ChatSideArea from '../components/ChatSideArea';
+import * as Auth from '../utils/auth';
+import { Navigate } from 'react-router-dom';
 
 export default function ChatPage() {
+
+  if(!Auth.loggedIn()) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className='d-flex chat-page'>
       <div className='d-flex h-100 w-100'>
