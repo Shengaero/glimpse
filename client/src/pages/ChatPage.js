20f4e282793bef5b20535ba9bd2ede4ddf10d33a
Kaidan Gustave <kaidangustave@yahoo.com>
2022-11-11 12:32:07 -0600

    hooked up message sending

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,13 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
 import Chat from '../components/Chat';
 import ChatSideArea from '../components/ChatSideArea';
+import ChatWebSocketProvider from '../components/ChatWebSocket';
+import { GET_ME } from '../utils/queries';
 
 export default function ChatPage() {
+  const { data } = useQuery(GET_ME);
+  const [chat, setChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  const handleNewMessage = ({ chatId, msg, userId, createdAt }) => {
+    // if the chatId the of the message matches the ID of the currently viewed chat
+    if(chatId === chat?._id) {
+      // find the index of the user that is sending the message
+      const i = chat.users.findIndex(({ _id }) => _id === userId);
+      // construct a new message object
+      const newMessage = {
+        content: msg,
+        author: chat.users[i],
+        createdAt: createdAt
+      };
+      // set messages with the new message at the top
+      setMessages([newMessage, ...messages]);
+    } else {
+      // TODO: Handle message sent in channel not currently viewing
+    }
+  };
+
+  useEffect(() => {
+    // if the data is available and there is no current chat sent
+    if(data && !chat) {
+      // get the first chat
+      const currentChat = data.me.chats[0]; // TODO: maybe set a chat ID in localhost and remember it?
+      // if the user has no chats, do not set this
+      if(currentChat) {
+        // set it as the current viewed chat
+        setChat(currentChat);
+      }
+    }
+  }, [data, chat, setChat]);
+
+  // if the data hasn't come in yet
+  if(!data) {
+    return <div>Loading</div>; // FIXME: Something that looks better.
+  }
+
   return (
     <div className='d-flex chat-page'>
       <div className='d-flex h-100 w-100'>
-        <ChatSideArea />
-        <Chat />
+        <ChatWebSocketProvider
+          wsURL="ws://localhost:3001/chat" // FIXME: fix this hardcoding
+          handleNewMessage={handleNewMessage}
+        >
+          <ChatSideArea />
+          {
+            chat ? (
+              <Chat
+                chatId={chat._id}
+                messages={messages}
+                setMessages={setMessages}
+              />
+            ) : (
+              <div>Loading...</div> // FIXME: add a default element for when there is no chat viewable
+            )
+          }
+        </ChatWebSocketProvider>
       </div>
     </div>
   );
