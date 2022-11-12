6c111aa0423dae55917adaae4ea7a5b06a73e2b0
Lux <anguyen780@gmail.com>
2022-11-12 12:57:30 -0600

    worked on adding offcanvas to gear icon and styled log out button

--- client/src/components/ChatSideArea.js
+++ client/src/components/ChatSideArea.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import ChatList from './ChatList';
-import { Navbar, Col, Button, Nav, Dropdown } from 'react-bootstrap';
+import { Navbar, Col, Button, Nav, Dropdown, Offcanvas, ListGroup } from 'react-bootstrap';
 import * as Auth from '../utils/auth';
 import CreateChatModal from './CreateChatModal';
 
 function ChatSideAreaNavbar() {
   const [showCreateChat, setShowCreateChat] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Navbar expand={false} className="px-2 py-0">
       <span className="fw-bold">GLIMPSE</span>
-      <Dropdown>
+      {/* <Dropdown>
         <Dropdown.Toggle>
           <i className="bi bi-gear-fill"></i>
         </Dropdown.Toggle>
@@ -20,7 +24,22 @@ function ChatSideAreaNavbar() {
           <Dropdown.Item href="#/action-2">Later in the future</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Just a placeholder for now</Dropdown.Item>
         </Dropdown.Menu>
-      </Dropdown>
+      </Dropdown> */}
+      <Button className='off-canvas-btn' onClick={handleShow}>
+        <i className="bi bi-gear-fill"></i>
+      </Button>
+      <Offcanvas className='off-canvas' show={show} onHide={handleClose}>
+        <Offcanvas.Header className='off-canvas-header'>
+        <i className="bi bi-x-lg close-btn" onClick={handleClose}></i>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ListGroup className='list-group-canvas'>
+            <ListGroup.Item className='create-chat' onClick={() => setShowCreateChat(true)}>Create Chat</ListGroup.Item>
+            <ListGroup.Item>Placeholder</ListGroup.Item>
+            <ListGroup.Item>Placeholder</ListGroup.Item>
+          </ListGroup>
+        </Offcanvas.Body>
+      </Offcanvas>
       <CreateChatModal show={showCreateChat} setShow={setShowCreateChat} />
     </Navbar>
   );
@@ -40,7 +59,7 @@ const ChatSideAreaNavBottom = () => (
       <img src='' alt='' />
       <span className="user">User</span>
     </div>
-    <Button className="py-0" onClick={() => {
+    <Button className="py-1" onClick={() => {
       Auth.logout();
     }}>
       Logout
